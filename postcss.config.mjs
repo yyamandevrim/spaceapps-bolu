043b35a90c979a6064e499f3281741e7ece88040
Yaman Devrim <yaman@devrim.biz.tr>
2025-07-30 21:21:15 +0300

    fix error

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -6,16 +6,6 @@ const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' && {
-      cssnano: {
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-          normalizeWhitespace: false,
-        }],
-      },
-    }),
   },
 };
 
