ece0b72c5bb7be9ab9de26381c4b5ded4e798750
Yaman Devrim <yaman@devrim.biz.tr>
2025-07-30 12:53:01 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "NASA SpaceApps Bolu"
+  title: "NASA Space Apps Bolu"
 }
 
 export default function RootLayout({
