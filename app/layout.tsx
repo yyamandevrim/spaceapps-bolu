8aa2aa7655a37eb0619d9865a31879e9da5d370e
Yaman Devrim <yaman@devrim.biz.tr>
2025-07-31 02:39:25 +0300

    add:favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,11 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "NASA Space Apps Bolu",
-    generator: 'v0.dev'
+  icons: {
+    icon: 'https://assets.spaceappschallenge.org/media/images/favicon2x.2e16d0ba.fill-192x192.png',
+    shortcut: 'https://assets.spaceappschallenge.org/media/images/favicon2x.2e16d0ba.fill-192x192.png',
+    apple: 'https://assets.spaceappschallenge.org/media/images/favicon2x.2e16d0ba.fill-192x192.png',
+  }
 }
 
 export default function RootLayout({
@@ -30,6 +34,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700;900&family=Overpass:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="https://assets.spaceappschallenge.org/media/images/favicon2x.2e16d0ba.fill-192x192.png" />
         <Analytics/>
       </head>
       <body className={inter.className}>{children}</body>
