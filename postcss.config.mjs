0cbca5f14db502a374cc3bc0b5b1746bc4f538d5
Yaman Devrim <yaman@devrim.biz.tr>
2025-07-30 21:14:33 +0300

    feat: remove commentlines on prod. env.s

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,21 @@
+//COPYRIGHT 2025, NASA SPACE APPS CHALLENGE BOLU
+// BY YAMANDEVRIM
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' && {
+      cssnano: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true,
+          },
+          normalizeWhitespace: false,
+        }],
+      },
+    }),
   },
 };
 
