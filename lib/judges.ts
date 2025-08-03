9d897cb6fd2fd542770bb3b55702df35a2423ff4
Yaman Devrim <yaman@devrim.biz.tr>
2025-08-03 18:10:10 +0300

    change judges to coming soon

--- lib/judges.ts
+++ lib/judges.ts
@@ -10,24 +10,24 @@ export const judges = [
     avatar: "/judges/okan-hoca.png",
   },
   {
-    name: "Prof. Michael Thompson",
-    role: "MIT Aerospace Engineering",
-    avatar: "https://picsum.photos/id/91/200/200",
+    name: "Çok Yakında!",
+    role: "",
+    avatar: "/logo-circular-dark.svg",
   },
   {
-    name: "Dr. Sarah Kim",
-    role: "JPL Mission Specialist",
-    avatar: "https://picsum.photos/id/64/200/200",
+    name: "Çok Yakında!",
+    role: "",
+    avatar: "/logo-circular-dark.svg",
   },
   {
-    name: "Dr. James Wilson",
-    role: "ESA Technology Director",
-    avatar: "https://picsum.photos/id/65/200/200",
+    name: "Çok Yakında!",
+    role: "",
+    avatar: "/logo-circular-dark.svg",
   },
   {
-    name: "Yaman Devrim",
-    role: "Lorem Ipsum Dolor",
-    avatar: "https://picsum.photos/id/40/200/200",
+    name: "Çok Yakında!",
+    role: "",
+    avatar: "/logo-circular-dark.svg",
   },
 ]
 
